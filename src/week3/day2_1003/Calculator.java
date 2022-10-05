1fe358ab65b608b446e3ad5e2470b83e5f2c14f6
ohsuk kwon <percykwon@gmail.com>
2022-10-05 10:15:49 +0900

    Calculator와 CaculatorMain으로 분리하기

---
+++ src/week3/day2_1003/Calculator.java
@@ -0,0 +1,22 @@
+package week3.day2_1003;
+
+public class Calculator {
+    public void plus(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public void minus(int a, int b) {
+
+        System.out.println(a - b);
+    }
+
+    public void multiple(int a, int b) {
+
+        System.out.println(a * b);
+    }
+
+    public void divide(int a, int b) {
+
+        System.out.println(a / b);
+    }
+}
---
+++ src/week3/day2_1003/CalculatorMain.java
@@ -0,0 +1,11 @@
+package week3.day2_1003;
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.plus(10,20);
+        calculator.minus(10,20);
+        calculator.multiple(10,20);
+        calculator.divide(10,20);
+    }
+}
