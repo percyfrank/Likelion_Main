340984a766c7a0651e12642095820c256f3daf41
ohsuk kwon <percykwon@gmail.com>
2022-10-06 14:59:44 +0900

    [221006] 파일 읽기 실습 1. 파일에서 n번째 글자 까지 읽어오기

--- src/week3/day3_1006/file_example/ReadFile.java
+++ src/week3/day3_1006/file_example/ReadFile.java
@@ -5,15 +5,20 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ReadFile {
 
     public static void main(String[] args) throws IOException {
 
-        FileReader fileReader = new FileReader("./a_file.txt");
+        Scanner sc = new Scanner(System.in);
+        int addnum = sc.nextInt();
 
-        char c = (char) fileReader.read();
+        ReadFileOne readFileOne = new ReadFileOne("./a_file.txt",addnum);
+        char c = (char) readFileOne.readOneByte();
         System.out.println(c);
+
+
 
     }
 
---
+++ src/week3/day3_1006/file_example/ReadFileOne.java
@@ -0,0 +1,26 @@
+package week3.day3_1006.file_example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFileOne {
+    private String filename;
+    private int n;
+
+    public ReadFileOne(String filename, int n) {
+        this.filename = filename;
+        this.n = n;
+    }
+
+    char readOneByte() throws IOException {
+        BufferedReader br = new BufferedReader(
+                new FileReader(filename),
+                16 * 1024
+        );
+        for(int i = 1; i < n; i++) {
+            System.out.println((char) br.read());
+        }
+        return (char)br.read();
+    }
+}
