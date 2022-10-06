e041c5a8735a20a6204fbcce9f3fbf30172332bd
ohsuk kwon <percykwon@gmail.com>
2022-10-06 10:18:49 +0900

    [221006] 랜덤한 숫자 중복제거 #2

--- src/week3/day3_1006/set_example/RndNumberWithoutDuplicated.java
+++ src/week3/day3_1006/set_example/RndNumberWithoutDuplicated.java
@@ -1,13 +1,20 @@
 package week3.day3_1006.set_example;
 
+import java.util.HashSet;
+
 public class RndNumberWithoutDuplicated {
     public static void main(String[] args) {
 
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
 
+        HashSet<Integer> studentsWithoutDuplicated = new HashSet<>();
+
+
+        int cnt = 0;
         for(int i = 0; i < 50 ; i++) {
-            System.out.println(randomNumberGenerator.generate(10));
+            studentsWithoutDuplicated.add(randomNumberGenerator.generate(10));
         }
+        System.out.println(studentsWithoutDuplicated);
 
     }
 
