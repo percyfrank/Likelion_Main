7fe9aaf1532f88f09516bede2b021d8cf38d802a
ohsuk kwon <percykwon@gmail.com>
2022-10-06 13:45:12 +0900

    [221006] map 실습 1.알파벳 개수 세기

--- src/week3/day3_1006/map_example/AlphabetCnt.java
+++ src/week3/day3_1006/map_example/AlphabetCnt.java
@@ -2,24 +2,34 @@ package week3.day3_1006.map_example;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class AlphabetCnt {
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String s = br.readLine();
+        String s = br.readLine().toUpperCase();
         AlphabetCnt alphabetCnt = new AlphabetCnt();
+        HashMap<Character, Integer> alphabetMap = new HashMap<>();
 
-        for(int i = 0; i < s.length(); i++){
-            if(alphabetCnt.isAlphabet(s.charAt(i)) == true) {
-
-            } else {
-                System.out.println("알파벳이 아닙니다.");
-                break;
-            }
+        for(char c = 'A'; c < 'Z'; c++) {
+            alphabetMap.put(c, 0);
         }
 
+//        Iterator it = alphabetMap.entrySet().iterator();
+//        Map.Entry entry = (Map.Entry)it;
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            boolean isAlphabet = alphabetCnt.isAlphabet(c);
+            if (isAlphabet == true) {
+                alphabetMap.put(c, alphabetMap.get(c) + 1);
+            }
+        }
+        System.out.println(alphabetMap);
     }
 
 
@@ -27,9 +37,10 @@ public class AlphabetCnt {
 
         if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
             return true;
-        else
+        else {
+            System.out.println("알파벳이 아닙니다.");
             return false;
+        }
     }
-
 
 }
