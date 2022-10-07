45eb37e035bfc34907add5d7d18dc9c3958533ef
ohsuk kwon <percykwon@gmail.com>
2022-10-07 09:13:07 +0900

    [221006] map 실습 #2 1. 알파벳 개수 출력 수정

--- src/week3/day3_1006/map_example/CountAlphabet.java
+++ src/week3/day3_1006/map_example/CountAlphabet.java
@@ -1,40 +1,35 @@
 package week3.day3_1006.map_example;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
-public class CountAlphabet {
-    public static void main(String[] args) throws Exception {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String s = br.readLine();
-
-//        HashMap<Character, Integer> map = new HashMap<>();
-        int[] arr = new int[26];
-
-        for (int i = 0; i < 26; i++) {
-//            map.put((char)(i+'a'),0);
-        }
-
-//        System.out.println(map);
-//        Iterator it = map.entrySet().iterator();
-//        Map.Entry entry = (Map.Entry)it;
-
-        for (int i = 0; i < s.length(); i++) {
-                arr[s.charAt(i) - 'a']++;
-//                if(s.charAt(i) == entry.getKey()) {
-//                    map.put((char)(j + 'a'), map.getOrDefault((char)(j + 'a'),0) + 1);
-        }
-
-
-        for (int i = 0; i < 26; i++) {
-            System.out.println("알파벳 " + (char)(i + 'a') + "의 개수는 : " + arr[i]);
-        }
-
-
-    }
-}
+//
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//import java.util.HashMap;
+//import java.util.Iterator;
+//import java.util.Map;
+//
+//public class CountAlphabet {
+//
+//    private HashMap<Character, Integer> alphabetMap;
+//    private String s;
+//
+//    public static void main(String[] args) throws Exception {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        for(char c = 'A'; c < 'Z'; c++) {
+//            alphabetMap.put(c, 0);
+//        }
+//
+//        AlphabetCnt alphabetCnt = new AlphabetCnt();
+//        for(int i = 0; i < s.length(); i++) {
+//            char c = s.charAt(i);
+//            boolean isAlphabet = alphabetCnt.isAlphabet(c);
+//            if (isAlphabet) {
+//                alphabetMap.put(c,alphabetMap.getOrDefault(c,0)+1);
+//            }
+//        }
+//    }
+//
+//    public CountAlphabet(HashMap<Character, Integer> alphabetMap, String s) {
+//        this.alphabetMap = alphabetMap;
+//        this.s = s;
+//    }
+//}
