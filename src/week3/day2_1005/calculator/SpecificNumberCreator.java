1ca79df0c38468082c1907d7339435b14baae442
ohsuk kwon <percykwon@gmail.com>
2022-10-07 09:08:32 +0900

    Random한 숫자로 사칙연산하기 1. SpecificNumberCreator 클래스 추가

---
+++ src/week3/day2_1005/calculator/SpecificNumberCreator.java
@@ -0,0 +1,8 @@
+package week3.day2_1005.calculator;
+
+public class SpecificNumberCreator implements NumberCreator {
+    @Override
+    public int randomNumber() {
+        return 1100000;
+    }
+}
