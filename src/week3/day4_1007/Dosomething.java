8e236a3b60a40161ef66024b19732e4ea8787ca2
ohsuk kwon <percykwon@gmail.com>
2022-10-07 13:37:26 +0900

    [221007] 대용량 데이터 처리 실습 1. 인터페이스 시도

---
+++ src/week3/day4_1007/Dosomething.java
@@ -0,0 +1,9 @@
+package week3.day4_1007;
+
+import java.util.List;
+
+public interface Dosomething<T> {
+
+    List<T> work(String str);
+
+}
---
+++ src/week3/day4_1007/SoutDoSomething.java
@@ -0,0 +1,10 @@
+package week3.day4_1007;
+
+import java.util.List;
+
+public class SoutDoSomething implements Dosomething{
+    @Override
+    public List<PopulationMove> work(String str) {
+        return null;
+    }
+}
