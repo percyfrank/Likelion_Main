978cad76185f746cd9b58fe6e9ae979e3887d514
ohsuk kwon <percykwon@gmail.com>
2022-10-18 09:45:44 +0900

    [221018] ADD : 재귀를 이용한 별 찍기

---
+++ src/week5/day19_1018/RecursiveStar.java
@@ -0,0 +1,26 @@
+package week5.day19_1018;
+
+import java.util.Scanner;
+
+public class RecursiveStar {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        RecursiveStar recursiveStar = new RecursiveStar();
+        recursiveStar.printRecursive(sc.nextInt());
+    }
+
+    public void Recursive(int n) {
+        if(n <= 0) return;
+        Recursive(n-1);
+        System.out.printf("*");
+    }
+    public void printRecursive(int n) {
+
+        if(n <= 0) return;
+        printRecursive(n-1);
+        Recursive(n);
+        System.out.println();
+        return;
+    }
+}
