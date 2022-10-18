8088bcf554d7d8fda90f17c665122d14d48551de
ohsuk kwon <percykwon@gmail.com>
2022-10-18 09:22:18 +0900

    [221018] ADD : 별 찍기 1. 정사각형, 직사각형

---
+++ src/week5/day19_1018/SquareStar.java
@@ -0,0 +1,32 @@
+package week5.day19_1018;
+
+import java.util.Scanner;
+
+public class SquareStar {
+
+    public void printRectangle(int a, int b) {
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printSquare(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        SquareStar squareStar = new SquareStar();
+        squareStar.printSquare(sc.nextInt());
+        squareStar.printRectangle(sc.nextInt(),sc.nextInt());
+    }
+}
