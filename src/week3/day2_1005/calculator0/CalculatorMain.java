7abb11a77997c448e535abaf709a0804b0a8196e
ohsuk kwon <percykwon@gmail.com>
2022-10-21 16:51:47 +0900

    [221005] UPDATE : CalculatorMain 리팩토링

--- src/week3/day2_1005/calculator0/CalculatorMain.java
+++ src/week3/day2_1005/calculator0/CalculatorMain.java
@@ -1,7 +1,5 @@
 package week3.day2_1005.calculator0;
 
-import week3.day2_1005.calculator0.Calculator;
-
 public class CalculatorMain {
     public static void main(String[] args) {
         Calculator calculator = new Calculator(10,20);
