28de57f64136ab97b67f0d20b18eb8fd1b18e920
ohsuk kwon <percykwon@gmail.com>
2022-10-28 11:02:03 +0900

    [221028] ADD : 전화번호 목록 최종

--- src/week6/day26_1028/PhoneBook.java
+++ src/week6/day26_1028/PhoneBook.java
@@ -5,23 +5,17 @@ import java.util.HashSet;
 public class PhoneBook {
 
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
 
         HashSet<String> hs = new HashSet<>();
-        for(var num : phone_book) {
-            hs.add(num);
-            for(String s : hs.) {
+        for(var num : phone_book) hs.add(num);
 
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = 0; j < phone_book[i].length(); j++) {
+                if (hs.contains(phone_book[i].substring(0, j))) return false;
             }
         }
-
-
-
-
-
-        return answer;
+        return true;
     }
-
 
 
     public static void main(String[] args) {
