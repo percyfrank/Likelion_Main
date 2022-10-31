727cc287647d87625cda5033b9f22a0c635cd3d3
ohsuk kwon <percykwon@gmail.com>
2022-10-31 17:55:28 +0900

    [221031] ADD : 모의고사 풀이 최종

--- src/week7/day27_1031/PrepareTest.java
+++ src/week7/day27_1031/PrepareTest.java
@@ -1,5 +1,9 @@
 package week7.day27_1031;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PrepareTest {
     /**
      * 1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
@@ -9,28 +13,49 @@ public class PrepareTest {
      * 1 2 3 4 5
      */
 
-    static int[] arr1 = {1, 2, 3, 4, 5};
-    static int[] arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
-    static int[] arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-    public int[] solution(int[] answers) {
+    static int[][] pattern = {{1, 2, 3, 4, 5},{2, 1, 2, 3, 2, 4, 2, 5},{3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
 
+    public int calculate(int[] answers, int[] pattern) {
         int cnt = 0;
+
         for (int i = 0; i < answers.length; i++) {
-            if(answers[i] == arr1[i]) {
+            if(answers[i] == pattern[i % pattern.length]) {
                 cnt++;
             }
         }
-        int[] answer = {};
-        return answer;
+        return cnt;
     }
 
+    public int[] solution(int[] answers) {
 
+        int maxcnt = 0;
+        int cnt[] = new int[3];
+        for (int i = 0; i < pattern.length; i++) {
+            if(calculate(answers,pattern[i]) >= maxcnt) {
+                maxcnt = calculate(answers, pattern[i]);
+                cnt[i] = maxcnt;
+            }
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < cnt.length; i++) {
+            if(maxcnt == cnt[i]) list.add(i+1);
+        }
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
 
     public static void main(String[] args) {
 
         int[] answers = {1, 2, 3, 4, 5};
+//        int[] answers = {1, 3, 2, 4, 2};
         PrepareTest pt = new PrepareTest();
-        System.out.println(pt.solution(answers));
+
+        System.out.println(Arrays.toString(pt.solution(answers)));
     }
 
 
