b9a09bbf786cf173f835af721dcc8afc880500fb
ohsuk kwon <percykwon@gmail.com>
2022-10-31 17:56:26 +0900

    [221028] UPDATE : 전화번호 목록 주석 추가

--- src/week6/day26_1028/PhoneBook.java
+++ src/week6/day26_1028/PhoneBook.java
@@ -9,6 +9,11 @@ public class PhoneBook {
         HashSet<String> hs = new HashSet<>();
         for(var num : phone_book) hs.add(num);
 
+        /**
+         * 전화번호부 전체를 set에 넣고, set에 있는 전화번호가
+         * 각 전화번호의 길이만큼 substring 했을 때 포함하는지 여부를 확인한다.
+         * O(N^2) 에서 O(20N)으로 줄어듬
+         */
         for (int i = 0; i < phone_book.length; i++) {
             for (int j = 0; j < phone_book[i].length(); j++) {
                 if (hs.contains(phone_book[i].substring(0, j))) return false;
@@ -16,7 +21,6 @@ public class PhoneBook {
         }
         return true;
     }
-
 
     public static void main(String[] args) {
 
