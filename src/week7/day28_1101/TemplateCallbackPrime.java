3ebe1f1f4960c3057c3f609c8a956419c0f2a80b
ohsuk kwon <percykwon@gmail.com>
2022-11-01 11:02:40 +0900

    [221101] UPDATE : Template Callback 수정 최종

--- src/week7/day28_1101/TemplateCallbackPrime.java
+++ src/week7/day28_1101/TemplateCallbackPrime.java
@@ -7,7 +7,8 @@ interface StatementStrategy {
 public class TemplateCallbackPrime {
 
     boolean isPrime(int num,StatementStrategy stmt) {
-        for (int i = 2;stmt.compare(i,num) ; i++) {
+        for (int i = 2; stmt.compare(i,num) ; i++) {
+            System.out.println(i);
             if(num % i == 0) return false;
         }
         return true;
@@ -16,7 +17,18 @@ public class TemplateCallbackPrime {
     public static void main(String[] args) {
 
         TemplateCallbackPrime tcp = new TemplateCallbackPrime();
-        boolean r = tcp.isPrime(13);
+        boolean r = tcp.isPrime(13, (a, b) -> a < b);
+        boolean r1 = tcp.isPrime(13, (a, b) -> a * a < b);
+        boolean r2 = tcp.isPrime(13, (a, b) -> a < b / 2);
+        boolean r3 = tcp.isPrime(13, new StatementStrategy() {
+            @Override
+            public boolean compare(int a, int b) {
+                return a < b;
+            }
+        });
         System.out.println(r);
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
     }
 }
