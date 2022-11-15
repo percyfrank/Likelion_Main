47c19a7d8124733458a60e1828ca9c3deaa2397c
ohsuk kwon <percykwon@gmail.com>
2022-11-15 11:00:09 +0900

    ADD : 퀵 정렬 ver2

---
+++ src/week9/day37_1115/QuickSort2.java
@@ -0,0 +1,48 @@
+package week9.day37_1115;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuickSort2 {
+
+    public static List<Integer> merge(List<Integer> left,List<Integer> mid,List<Integer> right) {
+
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.addAll(left);
+        answer.addAll(mid);
+        answer.addAll(right);
+        return answer;
+    }
+
+    public static List<Integer> sort(List<Integer> arr) {
+
+        if(arr.size() <= 1) return arr;
+
+        int pivot = arr.get(arr.size() / 2);
+
+        ArrayList<Integer> left = new ArrayList<>();
+        ArrayList<Integer> mid = new ArrayList<>();
+        ArrayList<Integer> right = new ArrayList<>();
+
+        for (int i = 0; i < arr.size(); i++) {
+            if(pivot > arr.get(i)) left.add(arr.get(i));
+            else if(pivot < arr.get(i)) right.add(arr.get(i));
+            else mid.add(arr.get(i));
+        }
+
+        return merge(sort(left), mid, sort(right));
+
+    }
+
+
+    public static void main(String[] args) {
+        var arr = new int[]{20, 18, 5, 19, 5, 25, 40, 50};
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
+        }
+        System.out.println(sort(list));
+
+    }
+}
