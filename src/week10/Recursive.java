387d411e29f20b7d5c2c5b13330500006e228a1b
ohsuk kwon <percykwon@gmail.com>
2022-11-22 09:41:09 +0900

    ADD : 재귀 예시

---
+++ src/week10/Recursive.java
@@ -0,0 +1,14 @@
+package week10;
+
+public class Recursive {
+
+    public static void print1To100(int num) {
+        if(num > 100) return;
+        System.out.println(num);
+        print1To100(num+1);
+    }
+    public static void main(String[] args) {
+        print1To100(1);
+
+    }
+}
