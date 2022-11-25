e4af764b21c4935501e64011c5933d57a7633ce5
ohsuk kwon <percykwon@gmail.com>
2022-11-25 11:14:00 +0900

    UPDATE FILENAME

--- src/week10/day44_1125/GCDWithRecursion.java
+++ src/week10/day44_1125/GCDWithRecursion.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class GCD_ver2 {
+public class GCDWithRecursion {
 
     /**
      * gcd(196, 42) = gcd(196 - 42, 42)
